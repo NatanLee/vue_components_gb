747fa11052bca229d7c5cabb2191030916b70dfd
NatanLee <afkolya@mail.ru>
2021-02-15 23:34:35 +0300

    lesson 7 0-38

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const a = (b) => {
-    return 10 + b;
-};
+//импорты
+const c = require('./func/');
+const b = require('./func');
+//встроенные
+const os = require('os');
+const fs = require('fs');
 
-console.log(a(10));
+const users = [{name: 'John', age: 20}];
+fs.writeFile('db.json', JSON.stringify(users), (err) => {
+    if(err){
+        console.log(err);
+    }    
+});
+
+// console.log(os.platform());
+// console.log(os.cpus());
+// console.log(os.type());
+
