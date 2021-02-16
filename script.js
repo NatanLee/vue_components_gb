9dfeac201c62252526e86c2b45efc426c15a6272
NatanLee <afkolya@mail.ru>
2021-02-16 16:18:52 +0300

    lesson 7 1-20

--- script.js
+++ script.js
@@ -4,13 +4,56 @@ const b = require('./func');
 //встроенные
 const os = require('os');
 const fs = require('fs');
+const http = require('http');
+const moment = require('moment');
 
-const users = [{name: 'John', age: 20}];
-fs.writeFile('db.json', JSON.stringify(users), (err) => {
+console.log(moment());
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.write('Hello world');
+        res.end();
+    }
+});
+
+server.listen('3000');
+server.on('connection', (socket) => {
+    console.log('New connection2');
+});
+
+
+
+
+
+
+
+
+
+/* const users = [{name: "Ann", age: 20}];
+const user1 = '{"name": "Jhon", "age": 30}'; */
+
+/* fs.readFile('db.json', 'utf-8', (err, data) =>
+    {if(err){
+        console.log(err);
+    }else{
+        const list = JSON.parse(data);
+        list.push(JSON.parse(user1));
+        fs.writeFile('db.json', JSON.stringify(list), (err) => {
+            if(err){
+                console.log(err);
+            }    
+        });
+    }    
+});
+ */
+    
+  
+
+/* fs.writeFile('db.json', JSON.stringify(users), (err) => {
     if(err){
         console.log(err);
     }    
-});
+}); */
 
 // console.log(os.platform());
 // console.log(os.cpus());
